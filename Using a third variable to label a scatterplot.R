a13191e01b5c73800221079d9886768d0a5f24e1
Patrick Mensah <70266683+patmensa39@users.noreply.github.com>
2022-07-21 11:00:08 -0400

    Add files via upload

---
+++ Using a third variable to label a scatterplot.R
@@ -0,0 +1,18 @@
+### USING A THIRD VARIABLE TO LABEL A SCATTERPLOT 
+
+data<- read.table("pgr.txt", header = TRUE)
+View(data)
+attach(data)
+names(data)
+
+plot(hay, pH, main = "Scatterplot of Hay versus Soil pH ", pch = 20 ,col = rainbow(8), 
+     xlab = "Total Hay", ylab = "Soil pH")
+
+text(hay, pH, labels = round(FR, 1), pos = 1, offset = 0.5, cex = 0.7)
+
+
+median(data$FR)
+
+plot(hay, pH, pch = 20, col = ifelse(FR > median(FR), "green", "black"), 
+     main = "Scatterplot of Hay versus Soil pH ")
+legend(locator(1), c("FR > median" , "FR = median"), pch = 20, col = c("green", "black"))
